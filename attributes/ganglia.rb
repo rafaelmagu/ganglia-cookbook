f6bcc01a4881bcbe92e73832232586c3489783b7
Nathan L Smith <nlloyds@gmail.com>
2011-08-30 09:40:53 -0500

    ganglia attr updates

--- attributes/ganglia.rb
+++ attributes/ganglia.rb
@@ -2,6 +2,7 @@ default[:ganglia][:version] = "3.1.7"
 default[:ganglia][:uri] = "http://sourceforge.net/projects/ganglia/files/ganglia%20monitoring%20core/3.1.7/ganglia-3.1.7.tar.gz/download"
 default[:ganglia][:checksum] = "bb1a4953"
 default[:ganglia][:cluster_name] = "default"
+default['ganglia']['location'] = 'unspecified'
 default[:ganglia][:network_interface] = 'eth0'
 default[:ganglia][:web][:version] = '2.1.6'
 default[:ganglia][:web][:graph_engine] = 'flot'
