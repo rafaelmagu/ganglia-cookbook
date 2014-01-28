1c0662da9e80b52274486528394df835c8de7171
Rafael Fonseca <rafael@bitsignal.com>
2014-01-29 12:09:33 +1300

    Fixed readme

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Heavy Water Software Inc."
 maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
 description      "Installs/Configures ganglia"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.3"
 
 %w{ debian ubuntu redhat centos fedora }.each do |os|
